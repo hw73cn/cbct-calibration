767aff1dde5b6917419ad36a8a4c984fa6f67cbc
Rholais Lii <rholais@gmail.com>
2016-10-25 09:16:47 +0800

    Modify standard deviation of angles

--- src/ftrGen.m
+++ src/ftrGen.m
@@ -23,9 +23,9 @@
 
 	lbl(1, :) = normrnd(200, 1, [1, NUM]);
 	lbl(2, :) = normrnd(400, 1, [1, NUM]);
-	ang(1, :) = normrnd(0, 1, [1, NUM]);
-	ang(2, :) = normrnd(0, 1, [1, NUM]);
-	ang(3, :) = normrnd(0, 1, [1, NUM]);
+	ang(1, :) = normrnd(0, 5, [1, NUM]);
+	ang(2, :) = normrnd(0, 5, [1, NUM]);
+	ang(3, :) = normrnd(0, 5, [1, NUM]);
 
 	for i = 1:6
 		bbN = 2 * i + 6;
